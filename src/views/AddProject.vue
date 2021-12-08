8bf703de204ba9bb132851eddafbf47cc4856e65
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-12-08 18:08:02 +0100

    added form to addProject

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,1 +1,22 @@
-vue
+<template>
+  <form>
+      <label>Title: </label>
+      <input type="text" required >
+    <label>Details: </label>
+    <textarea required> </textarea>
+ </form>
+</template>
+
+<script>
+export default {
+ data(){
+     return {
+     title:'',
+     details:''
+ }
+}
+</script>
+
+<style>
+
+</style>
